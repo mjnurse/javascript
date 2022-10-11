aa536b420764eb4efc2c36d759c0df7239b1daa6
Martin <martinnurse@quantexa.com>
2022-10-11 15:37:55 +0100

    Various

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -3,7 +3,17 @@
 function copyToClipboard(text) {
   const el = document.createElement('textarea');
   el.value =
-    ('gvim C:/MJN/github/mjnurse-github-io/' + text).replace(/\//g, '\\');
+    ('gvim C:/MJN/github/drive/mjnurse-github-io/' + text).replace(/\//g, '\\');
+  alert(el.value + ' copied to clipboard');
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
+function copyPageNameToClipboard(text) {
+  const el = document.createElement('textarea');
+  el.value =
+    ('gvim C:/MJN/drive/github/mjnurse-github-io/' + text).replace(/\//g, '\\');
   alert(el.value + ' copied to clipboard');
   document.body.appendChild(el);
   el.select();
